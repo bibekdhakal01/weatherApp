3bb693e41778d1a93933e84a298d36b7d031ade3
BibekDhakal <bibekdkl1122@gmail.com>
2026-01-03 19:46:28 +0545

    API integration

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const cityInput  = document.querySelector('.city-input');
 const getWeatherBtn = document.querySelector('.get-weather-btn');
 const apiKey = '1dbfe771cc191463fb6dfa8f5970594a'
 
+const weatherInfoSection = document.querySelector('.weather-info');
+const notFoundSection = document.querySelector('.not-found');
+const searchCitySection = document.querySelector('.search-city');
+
 getWeatherBtn.addEventListener('click', ()=>{   
     if(cityInput.value.trim() != ''){
     updateWeatgherInfo(cityInput.value)
@@ -19,11 +23,27 @@ cityInput.addEventListener('keydown', (event)=>{
     }
 })
 
-function  getFetchData(){
-    const apiUrl ='https://api.openweathermap.org/data/2.5/forcast?q={city name}&appid=$(apiKey)'
+async function  getFetchData(endPoint, city){
+    const apiUrl =`https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`
+
+    const response = await fetch(apiUrl)
+
+    return response.json()
 
 }
-function updateWeatgherInfo(city){
-    const weatherData =  getFetchData()
+async function updateWeatgherInfo(city){
+    const weatherData =  await getFetchData('weather', city);
+
+    if(weatherData.cod != 200){
+        showDisplaySection(notFoundSection)
+        return
+    }
+    showDisplaySection(weatherInfoSection)
+}
+
+function showDisplaySection(section){
+    [weatherInfoSection, notFoundSection, searchCitySection].forEach(section => section.computedStyleMap.display = 'none');
+    
+    section.style.display = 'flex';
 
 }
